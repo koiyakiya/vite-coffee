59172517f4a2f933015ab75cb595135fe03df6f4
koiyakiya <koiyakiya9@gmail.com>
2024-07-23 01:14:57 -0500

    84

--- src/components/Kofi.vue
+++ src/components/Kofi.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="kofi-button-wrapper">
-      <a :href="kofiURL" target="_blank" class="kofi-button" :style="buttonStyle">
-        <img class="kofi-icon" src="../assets/kofilogo.png" :style="iconStyle" />
-        <span class="kofi-text" :style="textStyle">Support me on Ko-fi</span>
+      <a :href="kofiURL" target="_blank" class="kofi-button">
+        <img class="kofi-icon" src="../assets/kofilogo.png" />
+        <span class="kofi-text">Support me on Ko-fi</span>
       </a>
     </div>
   </template>
@@ -18,46 +18,37 @@
       kofiURL() {
         return `https://ko-fi.com/${this.username}`;
       },
-      buttonStyle() {
-        return {
-          display: 'inline-flex',
-          alignItems: 'center',
-          backgroundColor: '#13233F',
-          color: 'white',
-          padding: '2px 12px',
-          borderRadius: '4px',
-          textDecoration: 'none',
-          fontFamily: '"Helvetica", "Arial", sans-serif',
-          fontSize: '13px',
-          fontWeight: '400',
-          lineHeight: '24px',
-        };
-      },
-      iconStyle() {
-        return {
-          width: '35px',
-          height: '35px',
-          marginRight: '8px',
-        };
-      },
-      textStyle() {
-        return {
-          color: 'white',
-        };
-      },
     },
   });
   </script>
   
   <style scoped>
-  .kofi-button-wrapper :deep(.kofi-button) {
-    all: initial;
-    * {
-      all: unset;
-    }
+  .kofi-button {
+    display: inline-flex !important;
+    align-items: center !important;
+    background-color: #13233F !important;
+    color: white !important;
+    padding: 2px 12px !important;
+    border-radius: 4px !important;
+    text-decoration: none !important;
+    font-family: "Helvetica", "Arial", sans-serif !important;
+    font-size: 13px !important;
+    font-weight: 400 !important;
+    line-height: 24px !important;
+  }
+  
+  .kofi-icon {
+    width: 35px !important;
+    height: 35px !important;
+    margin-right: 8px !important;
+  }
+  
+  .kofi-text {
+    color: white !important; 
   }
   
   .kofi-button-wrapper :deep(.kofi-button:hover) {
     background-color: #1e3a6a !important;
   }
   </style>
+  
